eaae177cac6c27a09620501aa2db4f9e504d32af
Prisyazhnyuk Alexander <prisyagnyuk@gmail.com>
2015-06-17 02:49:31 +0300

    init

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/websocket"
+	"log"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	c := &connection{send: make(chan []byte, 256), ws: conn}
+	h.register <- c
+	go c.writePump()
+
+	log.Printf("New connection");
+	log.Printf("Count connections: %s", len(h.connections));
+
+	c.readPump()
+}
+
+func main() {
+	go h.run()
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/ws", handler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
---
+++ static/js/app.js
@@ -0,0 +1,36 @@
+var ws = null;
+var wsurl = "ws://127.0.0.1:8080/ws";
+
+window.onload = function() {
+    ws = new WebSocket(wsurl);
+
+    ws.onopen = function() {
+        //ws.send('ping');
+        console.log("connected to " + wsurl);
+        init();
+    };
+
+    ws.onclose = function(e) {
+        console.log("connection closed (" + e.code + ")");
+    };
+
+    ws.onmessage = function(e) {
+        var data = e.data;
+        console.log("message received: " + e.data);
+    };
+};
+
+function init() {
+    var ProtoBuf = dcodeIO.ProtoBuf;
+    var file = ProtoBuf.loadProtoFile("/proto/server_request.proto");
+    console.log(file);
+    var ServerRequest = file.build("messages.ServerRequest");
+    var ServerRequestPing = file.build("messages.ServerRequestPing");
+    var requestPing = new ServerRequestPing('ping request');
+    var request = new ServerRequest('pingmethod', requestPing);
+    console.log(request);
+    console.log(requestPing);
+    var buf = request.toArrayBuffer();
+
+    ws.send(buf);
+};
